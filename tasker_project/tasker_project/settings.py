59a64c836dc25e343013c67e66bd9d30cbe09d65
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-26 17:25:59 +0000

    update

--- tasker_project/tasker_project/settings.py
+++ tasker_project/tasker_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "tasker_app"
+    "tasker_app",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
