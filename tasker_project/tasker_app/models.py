791ad6095484acb11e2ddd690a99b6383e5affc9
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-26 17:28:58 +0000

    update

--- tasker_project/tasker_app/models.py
+++ tasker_project/tasker_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(default=False, null=True)
+    description = models.TextField(blank=True, null=True)
     is_complited = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
