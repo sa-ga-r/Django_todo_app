32e0c34d214b38f87bea7b492d4f723938c2a93f
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-27 15:30:47 +0000

    update

--- tasker_project/tasker_project/urls.py
+++ tasker_project/tasker_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('tasker_app.urls'))
+    path('api/', include('tasker_app.urls'))
 ]
