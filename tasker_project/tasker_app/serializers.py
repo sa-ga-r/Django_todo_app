1d4497bcd60a20977e94a6709e5e2ae07b716021
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-27 15:44:37 +0000

    update

--- tasker_project/tasker_app/serializers.py
+++ tasker_project/tasker_app/serializers.py
@@ -2,5 +2,6 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
-    model = Task
-    fields = '__all__'
+    class meta:
+        model = Task
+        fields = '__all__'
