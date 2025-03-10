3993e874f68f822ef7771ab3b374aaec83455340
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-03-10 09:05:12 +0000

    update

--- tasker_project/tasker_front/static/app.js
+++ tasker_project/tasker_front/static/app.js
@@ -21,7 +21,7 @@ function updateTable(data){
         const statusCell = document.createElement("td");
         statusCell.textContent = task.is_complited ? "DONE" : "PENDING";
         const actCell = document.createElement("td");
-        actCell.style.textAlign = "center";
+        actCell.style.marginRight = "10px";
         const editBtn = document.createElement("button");
         editBtn.textContent = "Edit";
         editBtn.style.marginRight= "4px";
